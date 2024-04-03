a4c09975a9d30eb8c378534a15932e1cf304dcbe
Ham <hamzahqureshi25@gmail.com>
2024-04-03 11:54:11 +0100

    bounded map viewport and implmented janky but ok zoon/pan

--- Ethnic_Groups_Eng_Wales/js/map.js
+++ Ethnic_Groups_Eng_Wales/js/map.js
@@ -10,6 +10,14 @@ export const map = (parent, props) => {
     const innerWidth = width - margin.left - margin.right;
     const innerHeight = height - margin.top - margin.bottom;
 
+    // Box defined by margins
+    parent.append('rect')
+        .attr('x', margin.left)
+        .attr('y', margin.top)
+        .attr('width', innerWidth)
+        .attr('height', innerHeight)
+        .attr('fill', '#69a3b2');
+
     // Define Projection Constants
     const projection = d3.geoNaturalEarth1()
         .center([-2, 53])
@@ -19,30 +27,41 @@ export const map = (parent, props) => {
     // Group to enter our map elements and facilitate zoom
     const map = parent.selectAll('.map').data([null])
     const mapEnter = map
+        .enter().append('svg')
+        .attr("viewBox", [margin.left, margin.top, margin.left+innerWidth, margin.top+innerHeight])
+        .attr("width", innerWidth)
+        .attr("height", innerHeight)
+        .attr("style", "max-width: 100%; height: auto;")
+        .attr('class', 'map');
+    const mapchild = mapEnter.selectAll('.mapchild').data([null])
+    const mapchildEnter = mapchild
         .enter().append('g')
-            .attr('transform', `translate(${margin.left},${margin.top})`)
-            .attr('class', 'map');
-    const gchild = mapEnter.append('g')
+        .attr('transform', `translate(${margin.left-150},${margin.top})`)
+            .attr('class', 'mapchild');
+            
 
     console.log([d3.min(data.map(d => d['Percent Non-WB'])), d3.max(data.map(d => d['Percent Non-WB']))])
+
+//    mapchildEnter.append('path')
+//        .attr('class', 'sphere')
+//        .attr('d', pathGenerator({type: 'Sphere'}));
 
     // Initialise scales
     const nonWBScale = d3.scaleLinear()
         .domain([0, d3.max(data.map(d => d['Percent Non-WB']))])
-        .range(["white","blue"])
+        .range(["white","green"])
 
     // d3-zoom 
     mapEnter.call(d3.zoom()
-        .scaleExtent([1, 10])
+        .scaleExtent([1, 15])
+        .extent([[margin.left-150, margin.top],[margin.left+innerWidth, margin.top+innerHeight]])
         .translateExtent([[0, 0], [width, height]])
-        .on('zoom', event => gchild.attr('transform', event.transform)));
+        .on('zoom', event => mapchildEnter.attr('transform', event.transform)));
 
     // Below contains all code that relies on the map data in some way
     d3.json(chosen_map)
         .then(mapdata => {
-            console.log(mapdata)
-
-            const mapShape = gchild.selectAll('path').data(mapdata.features)
+            const mapShape = mapchildEnter.selectAll('path').data(mapdata.features)
             const mapShapeEnter = mapShape
                 .enter().append('path')
                     .attr('class','country')
